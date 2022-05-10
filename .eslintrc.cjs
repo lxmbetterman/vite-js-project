3e40746c5149e82a093566213216f1d37fb8b57b
laixueming <747748443@qq.com>
2022-05-10 09:57:40 +0800

    [添加prettier规则][项目配置了prettier相关功能，会自动合并prettier规则到eslint规则中][重启生效]

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,18 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
   ],
   env: {
     // 解决’defineProps’ is not defined
-    "vue/setup-compiler-macros": true,
+    'vue/setup-compiler-macros': true,
   },
-};
+  rules: {
+    // semi: [2, "never"],
+  },
+}
