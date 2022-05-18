3d502d016f247e924c2e8354abbd0859c16088b1
laixueming <747748443@qq.com>
2022-05-18 15:29:14 +0800

    [调整prettier][依赖注入]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,26 @@
 <template>
   <main>
-    <TestComVue :title="'123?????'" propF="success">Slot!!</TestComVue>
+    <TestComVue :title="'123?????'" propF="success">
+      Slot!!
+      <template #test>testtt</template>
+    </TestComVue>
     <TheWelcome></TheWelcome>
   </main>
 </template>
 <script setup>
-  import TestComVue from './TestCom.vue'
-  import TheWelcome from '@/components/TheWelcome.vue'
+import TestComVue from './TestCom.vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+
+import { provide, ref } from 'vue'
+
+const location = ref('North Pole')
+
+function updateLocation() {
+  location.value = 'South Pole'
+}
+
+provide('location', {
+  location,
+  updateLocation,
+})
 </script>
-
-
--- src/views/TestCom.vue
+++ src/views/TestCom.vue
@@ -9,54 +9,55 @@
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
     />
+    <div></div>
+    <slot name="test"></slot>
   </div>
 </template>
 <script setup>
-  // s
-  const props = defineProps({
-    title: {
-      type: String,
+// s
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  propF: {
+    validator(value) {
+      // The value must match one of these strings
+      return ['success', 'warning', 'danger'].includes(value)
     },
-    propF: {
-      validator(value) {
-        // The value must match one of these strings
-        return ['success', 'warning', 'danger'].includes(value)
-      },
-    },
-    modelValue: Number,
-    modelModifiers: { default: () => ({}) },
-  })
+  },
+  modelValue: Number,
+  modelModifiers: { default: () => ({}) },
+})
 
-  // defineEmits(['update:modelValue'])
+// defineEmits(['update:modelValue'])
 
-  console.log(props.modelModifiers) // { capitalize: true }
-  console.log(props.title)
+console.log(props.modelModifiers) // { capitalize: true }
+console.log(props.title)
 
-  // const emit = defineEmits(['enlargeText'])
-  const emit = defineEmits({
-    // 没有校验
-    // click: null,
+// const emit = defineEmits(['enlargeText'])
+const emit = defineEmits({
+  // 没有校验
+  // click: null,
 
-    // 校验 submit 事件
-    submit: ({ email, password }) => {
-      if (email && password) {
-        return true
-      } else {
-        console.warn('Invalid submit event payload!')
-        return false
-      }
-    },
-    'update:modelValue': null,
-  })
-  console.log(props.modelModifiers, 'modelModifiers') // { capitalize: true }
+  // 校验 submit 事件
+  submit: ({ email, password }) => {
+    if (email && password) {
+      return true
+    } else {
+      console.warn('Invalid submit event payload!')
+      return false
+    }
+  },
+  'update:modelValue': null,
+})
+console.log(props.modelModifiers, 'modelModifiers') // { capitalize: true }
 
-  function submitForm(email, password) {
-    emit('submit', { email, password })
-  }
+function submitForm(email, password) {
+  emit('submit', { email, password })
+}
 
-  emit('enlarge-text')
+emit('enlarge-text')
 </script>
 // 模板
-
 
 <style lang="scss" scoped></style>
