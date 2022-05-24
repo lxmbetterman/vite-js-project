0045c2130f05296b5993976ac175b460eb448d57
laixueming <747748443@qq.com>
2022-05-24 17:04:05 +0800

    添加elementPLus UI库

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import myDirectives from '@/directive'
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(myDirectives, { test: '????' }) // options:{ test: '????' }
 app.use(router)
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button type="primary" size="small">？？</el-button>
     <TestCom></TestCom>
   </div>
 </template>
