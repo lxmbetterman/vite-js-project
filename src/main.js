f148e399bf8f47425f6cc665f720d44fa7db1ba2
laixueming <747748443@qq.com>
2022-05-25 10:54:43 +0800

    [主题替换需要的依赖包css-color-function][主题替换思路][@vueuse/core]

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+// import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import myDirectives from '@/directive'
@@ -11,5 +11,12 @@ app.use(ElementPlus)
 app.use(createPinia())
 app.use(myDirectives, { test: '????' }) // options:{ test: '????' }
 app.use(router)
+
+import { getIndexStyle, writeNewStyle } from '@/utils/theme/index'
+
+/* 初始化主题样式 */
+getIndexStyle().then(() => {
+  writeNewStyle()
+})
 
 app.mount('#app')
---
+++ src/utils/theme/index.js
@@ -0,0 +1,61 @@
+import { generateColors } from './color'
+import axios from 'axios'
+import formula from './formula.json'
+// import variables from '@/assets/scss/var.scss'
+// const variables = '#ff0000'
+
+let originalStyle = ''
+
+export function writeNewStyle() {
+  let colors = generateColors('#7A40F2') // 选择的主颜色
+
+  let cssText = originalStyle
+  let colorsCssText = ''
+  Object.keys(colors).forEach((key) => {
+    cssText = cssText.replace(
+      //  把颜色key替换成颜色值 # primary -> 409EFF
+      new RegExp('(:|\\s+)' + key, 'g'),
+      '$1' + `${colors[key]}`
+    )
+    colorsCssText += `
+      .color-${key}{color: ${colors[key]}!important;}
+      .bg-${key}{background-color: ${colors[key]}!important;}
+      .border-${key}{border-color: ${colors[key]}!important;}
+    `
+  })
+  document.getElementById('themeStyle').innerText = cssText + colorsCssText
+}
+
+export function getIndexStyle() {
+  return new Promise((resolve) => {
+    if (!originalStyle) {
+      axios
+        .all([axios.get('/theme/index.css'), axios.get('/theme/extra.css')])
+        .then(
+          axios.spread((file, extraFile) => {
+            const fileData = file.data
+            const extraFileData = extraFile.data.replace(/[\r\n]/g, '')
+            originalStyle = getStyleTemplate(fileData + extraFileData)
+            resolve()
+          })
+        )
+    } else {
+      resolve()
+    }
+  })
+}
+
+export function getStyleTemplate(data) {
+  let colors = generateColors('#409EFF')
+
+  const colorMap = new Map()
+  Object.keys(formula).forEach((key) => {
+    colorMap.set(colors[key], key) // 把颜色值替换成key #409EFF -> primary
+  })
+
+  for (let [key, value] of colorMap) {
+    data = data.replace(new RegExp(key, 'ig'), value)
+  }
+
+  return data
+}
